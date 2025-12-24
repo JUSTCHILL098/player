c50da65d4b660f0a1813c13b233421ed3cfe920e
JUSTCHILL098 <sandy2761910@gmail.com>
2025-12-24 13:34:38 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,19 @@
 import "./globals.css"
-import { GeistMono } from "geist/font/mono"
+import { Geist_Mono } from "next/font/google"
+
+const geistMono = Geist_Mono({
+  subsets: ["latin"],
+})
 
 export const metadata = {
   title: "MegaPlay API",
-  description: "Hianime Video API"
+  description: "Hianime Video API",
 }
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={GeistMono.className}>
-      <body>
+    <html lang="en">
+      <body className={geistMono.className}>
         {children}
       </body>
     </html>
