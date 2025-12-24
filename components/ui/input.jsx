22db4ffb2f034d580749f1b85e91aa3e9e89536b
JUSTCHILL098 <sandy2761910@gmail.com>
2025-12-24 13:21:54 +0530

    Create input.jsx

---
+++ components/ui/input.jsx
@@ -0,0 +1,10 @@
+import { cn } from "@/lib/utils"
+
+export function Input(props) {
+  return (
+    <input
+      className="w-full rounded-md bg-zinc-900 border px-3 py-2 text-sm"
+      {...props}
+    />
+  )
+}
